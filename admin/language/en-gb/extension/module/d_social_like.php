cec2930258d32fbd17a028a38d4b1dbef14e2891
dmitriyzhuk <dmitriyzhuk@gmail.com>
2018-09-10 19:04:16 +0300

    added latest version

---
+++ admin/language/en-gb/extension/module/d_social_like.php
@@ -0,0 +1,174 @@
+<?php
+// Heading
+$_['heading_title'] = '<span style="color:#449DD0; font-weight:bold">Social Like</span><span style="font-size:12px; color:#999"> by <a href="http://www.opencart.com/index.php?route=extension/extension&filter_username=Dreamvention" style="font-size:1em; color:#999" target="_blank">Dreamvention</a></span>';
+
+$_['heading_title_main'] = 'Social Like';
+
+// Text
+$_['text_edit']             = 'Edit Social Like settings';
+$_['text_modules']                = 'Modules';
+$_['text_settings']           = 'Settings';
+$_['text_design']           = 'Design';
+$_['text_instructions']         = 'Instructions';
+$_['text_success']            = 'Success: You have modified module Social Like!';
+$_['text_all_stores']             = 'All Stores';
+$_['text_all_languages']          = 'All Languages';
+$_['text_pro']                      = '<a href="https://dreamvention.ee/social-like-buttons-facebook-twitter-pintrest-google-etc">If you need facebook, twitter and the rest of popular social likes - upgrade to PRO now!</a>';
+$_['text_yes']                  = 'Yes';
+$_['text_no']             = 'No';
+$_['text_enabled']                = 'Enabled';
+$_['text_disabled']               = 'Disabled';
+$_['text_success_no_update']            = 'Super! You have the latest version.';
+$_['text_warning_new_update']           = 'Wow! There is a new version available for download.';
+$_['text_error_update']               = 'Sorry! Something went wrong. If this repeats, contact the support please.';
+$_['text_error_failed']               = 'Oops! We could not connect to the server. Please try again later.';
+$_['text_setup']            = '<style>
+                .welcome {
+                    background: url("view/image/d_social_like/bg.svg");
+                    background-position: bottom;
+                    background-repeat: repeat-x;
+                    background-size: 50%;
+                    min-height: 700px;
+                    padding-top: 50px;
+                    padding-bottom: 200px;
+                }
+
+                .welcome-into {
+                    text-align: center;
+                    max-width: 500px;
+                    margin: auto;
+                }
+
+                .welcome-into-logo {
+                    padding-bottom: 15px;
+                }
+
+                .welcome-into-heading {
+                    font-size: 30px;
+                    font-weight: bold;
+                    padding-bottom: 15px;
+                }
+
+                .welcome-into-text {
+                    font-size: 18px;
+                    padding-bottom: 30px;
+                }
+
+                .welcome-into-start {
+                    padding-bottom: 30px;
+                }
+
+                .welcome-features {
+                    padding: 30px;
+                }
+
+                .welcome-features-icon {
+                    padding: 20px;
+                    height: 130px;
+                    text-align: center;
+                }
+
+                .welcome-features-text {
+                    text-align: center;
+                    font-weight: bold;
+                    font-size: 16px;
+                }
+
+                .panel .panel-body {
+                    padding: 0px;
+                }
+            </style>
+            <div class="welcome" style="
+    padding-bottom: 250px;
+">
+                <div class="welcome-into">
+                    <div class="welcome-into-logo"><img src="view/image/d_social_like/logo.svg"/></div>
+                    <div class="welcome-into-heading">Social Like</div>
+                    <div class="welcome-into-text">70% of customers say they trust opinions posted on social networks. Social likes is a major part of your social proof. With this extension you will add social likes from facebook, twitter, linkedin and more and increase your sales. 
+                    </div>
+
+                    <div class="welcome-into-start">
+                        <a class="btn btn-primary btn-lg setup">Setup</a>
+                    </div>
+                
+                </div>
+            </div>';
+
+$_['text_google']               = 'Google+';
+$_['text_facebook']               = 'Facebook';
+$_['text_twitter']              = 'Twitter';
+$_['text_powered_by']     = 'Tested with <a href="https://shopunity.net/extension/ajax-quick-checkout-pro" target="_blank">Shopunity.net</a> <br> Find more amazing extensions at <a href="https://dreamvention.ee/" target="_blank">Dreamvention.ee</a>';
+$_['text_live']                 = 'Live';
+$_['text_linkedin']               = 'LinkedIn';
+$_['text_vkontakte']              = 'Vkontakte';
+$_['text_odnoklassniki']          = 'Odnoklassniki';
+$_['text_mailru']               = 'Mail.ru';
+$_['text_stumbleupon']            = 'Stumbleupon';
+$_['text_foursquare']             = 'Foursquare';
+$_['text_amazon']               = 'Amazon  <i class="icon-question" rel="tooltip" data-help="Amazone button saves the product to ypur wishlist on Amazone. This option will show only on a product page. You will not see it on other pages, even if you have activated it here."></i>';
+$_['text_pinterest']              = 'Pinterest';
+$_['text_addthis']              = 'Addthis';
+$_['text_ticket']         = 'Open Ticket';
+$_['entry_ticket']        = 'Support<br/><small>Create a ticket. If you find a bug, even in a free version, please let us know.</small>';
+$_['success_setup']             = 'Success: You have activated social like module! Check your homepage for social likes on the left side!';
+$_['text_view_left']          = 'Left';
+$_['text_view_top']           = 'Top';
+$_['text_view_right']           = 'Right';
+$_['text_view_bottom']          = 'Bottom';
+$_['text_view_inline']          = 'Inline';
+
+$_['text_sort_order']           = 'Sort order';
+
+// Entry
+$_['entry_get_update']            = 'You have version %s';
+$_['entry_name']                  = 'Name';
+$_['entry_language']          = 'Language';
+$_['entry_store']             = 'Store';
+$_['entry_status']                = 'Status';
+$_['entry_view']                    = 'View:';
+$_['entry_url']                   = 'Url to Like';
+$_['entry_config_files']        = 'Config files';
+
+$_['entry_config_settings']         = 'Predefined settings';
+$_['entry_icon_theme']            = 'Icon design ';
+$_['entry_icon_color']            = 'Icon';
+$_['entry_icon_color_active']       = 'Icon active';
+$_['entry_background_color']        = 'Background';
+$_['entry_background_color_active']   = 'On hover';
+
+$_['entry_api']             = '<a href="http://vk.com/dev/Like" target="_blank">Create Api</a>';
+$_['entry_width']             = 'Width';
+$_['entry_border']            = 'Turn on/off  border';
+$_['entry_border_color']        = 'Set border color';
+$_['entry_box_shadow_color']      = 'Set box shadow color';
+$_['entry_box_shadow']          = 'Turn on/off box shadow';
+$_['entry_border_radius']         = 'Turn on/off border radius';
+$_['entry_popup_mobile']        = 'Turn on/off popup for mobile';
+$_['entry_custom_style']        = 'Add custom styles <i class="icon-question" rel="tooltip" data-help="Sometimes you need to add cutome SCC styles to fix theme conglicts. This is the best place to do it."></i>';
+
+//Help
+$_['help_config_files']             = 'You can select from a list of predefined settings the one you like and then edit them.';
+$_['help_icon_theme']             = 'You can select another icon design for the two views of social likes: left and right. the rest of the views show the buttons directly without icons.';
+$_['help_width']            = 'Set the width of the like button. Some likebuttons have really wide blocks and with this option you can fit them nicely.';
+$_['help_api']              = 'You need to setup a widget for Vkontakte like buttons to work. Once created, place the id here. i.e. apiId: 3704523';
+$_['help_custom_style']         = 'Sometimes you need to add custom CSS styles to fix theme conflicts. This is the best place to do it.';
+$_['help_url']              = 'Please enter a URL which you want to like from all pages of your site.';
+
+// Button
+$_['button_save_and_stay']        = 'Save and Stay';
+$_['button_save']                 = 'Save';
+$_['button_cancel']               = 'Cancel';
+$_['button_get_update']           = 'Check for update';
+
+// Error
+$_['error_warning']               = 'Warning: Please check the form carefully for errors!';
+$_['error_permission']            = 'Warning: You do not have permission to modify module Social Like!';
+$_['error_name']                  = 'Warning: Module Name must be between 3 and 64 characters!';
+$_['text_not_found'] = '<div class="jumbotron">
+          <h1>Please install Shopunity</h1>
+          <p>Before you can use this module you will need to install Shopunity. Simply download the archive for your version of opencart and install it view Extension Installer or unzip the archive and upload all the files into your root folder from the UPLOAD folder.</p>
+          <p><a class="btn btn-primary btn-lg" href="https://shopunity.net/download" target="_blank">Download</a></p>
+        </div>';
+
+$_['success_twig_compatible']    = 'Twig support successfully installed.';
+?>
